b7eb29054bb8fa72fa348b7b7c7d445afea9b2ba
veronlkkkaa <veronlkkkaaa@mail.ru>
2025-10-12 14:58:57 +0300

    add CI workflow

--- src/lab1/euler21.clj
+++ src/lab1/euler21.clj
@@ -1,48 +1,57 @@
 (ns lab1.euler21)
 
-;; Быстрая версия sum-proper-divisors: перебираем до sqrt(n), добавляем пары делителей.
+;; Быстрая версия sum-proper-divisors:
+;; перебираем делители до sqrt(n), добавляем пары (i и n/i).
 (defn sum-proper-divisors [n]
   (cond
-    (<= n 1) 0
+    (<= n 1)
+    0
+
     :else
     (let [sqrt-n (long (Math/floor (Math/sqrt n)))]
-      (loop [i 2 sum 1]
+      (loop [i 2
+             sum 1]
         (if (> i sqrt-n)
           sum
           (if (zero? (mod n i))
             (let [other (quot n i)]
-              (recur (inc i) (if (= other i) (+ sum i) (+ sum i other))))
+              (recur (inc i)
+                     (if (= other i)
+                       (+ sum i)
+                       (+ sum i other))))
             (recur (inc i) sum)))))))
 
-; --- Решения ---
+;; --- Решения ---
 
-; 1. Монолитная реализация с хвостовой рекурсией
+;; 1. Монолитная реализация с хвостовой рекурсией
 (defn solve-tail-rec []
-  (loop [n 1 total 0]
+  (loop [n 1
+         total 0]
     (if (> n 9999)
       total
-      (let [d-n (sum-proper-divisors n)
-            d-d-n (sum-proper-divisors d-n)]
+      (let [d-n     (sum-proper-divisors n)
+            d-d-n   (sum-proper-divisors d-n)]
         (if (and (= d-d-n n)
                  (not= d-n n))
           (recur (inc n) (+ total n))
           (recur (inc n) total))))))
 
-; 2. Обычная рекурсивная версия
+;; 2. Обычная рекурсивная версия (на самом деле тоже хвостовая — идентична 1)
 (defn solve-rec []
-  (loop [n 1 total 0]
+  (loop [n 1
+         total 0]
     (if (> n 9999)
       total
-      (let [d-n (sum-proper-divisors n)
-            d-d-n (sum-proper-divisors d-n)]
+      (let [d-n     (sum-proper-divisors n)
+            d-d-n   (sum-proper-divisors d-n)]
         (if (and (= d-d-n n)
                  (not= d-n n))
           (recur (inc n) (+ total n))
           (recur (inc n) total))))))
 
-; 3. Модульный/функциональный подход
+;; 3. Модульный/функциональный подход
 (defn amicable? [n]
-  (let [d-n (sum-proper-divisors n)
+  (let [d-n   (sum-proper-divisors n)
         d-d-n (sum-proper-divisors d-n)]
     (and (= d-d-n n)
          (not= d-n n))))
@@ -52,26 +61,30 @@
        (filter amicable?)
        (reduce + 0)))
 
-; 4. Использование for
+;; 4. Использование for
 (defn solve-for []
-  (reduce
-   +
-   0
-   (for [n (range 1 10000)
-         :let [d-n (sum-proper-divisors n)
-               d-d-n (sum-proper-divisors d-n)]
-         :when (and (= d-d-n n)
-                    (not= d-n n))]
-     n)))
+  (reduce +
+          0
+          (for [n (range 1 10000)
+                :let [d-n   (sum-proper-divisors n)
+                      d-d-n (sum-proper-divisors d-n)]
+                :when (and (= d-d-n n)
+                           (not= d-n n))]
+            n)))
 
-; 5. Ленивые последовательности
-(defn lazy-amicables []
+;; 5. Ленивые последовательности
+(defn solve-lazy []
   (letfn [(amicable? [n]
-            (let [d-n (sum-proper-divisors n)
+            (let [d-n   (sum-proper-divisors n)
                   d-d-n (sum-proper-divisors d-n)]
               (and (= d-d-n n)
-                   (not= d-n n))))]
-    (filter amicable? (range 1 10000))))
+                   (not= d-n n))))
 
-(defn solve-lazy []
-  (reduce + 0 (lazy-amicables)))
+          (gen [n]
+            (lazy-seq
+             (cons n
+                   (gen (inc n)))))]
+    (->> (gen 1)
+         (take-while #(< % 10000))
+         (filter amicable?)
+         (reduce + 0))))
--- src/lab1/euler9.clj
+++ src/lab1/euler9.clj
@@ -5,30 +5,49 @@
 
 ;; 1. Хвостовая рекурсия
 (defn tail-rec []
-  (loop [a 1 b 2]
+  (loop [a 1
+         b 2]
     (when-not (> a 333)
       (let [c (- 1000 a b)]
         (cond
-          (> b c) (recur (inc a) (inc (inc a)))
-          (= (+ (* a a) (* b b)) (* c c)) (* a b c)
-          :else (recur a (inc b)))))))
+          (> b c)
+          (recur (inc a) (inc (inc a)))
+
+          (= (+ (* a a)
+                (* b b))
+             (* c c))
+          (* a b c)
+
+          :else
+          (recur a (inc b)))))))
 
 ;; 2. Обычная рекурсивная версия
 (defn solve-rec []
-  (loop [a 1 b 2]
+  (loop [a 1
+         b 2]
     (when-not (> a 333)
       (let [c (- 1000 a b)]
         (cond
-          (> b c) (recur (inc a) (inc a))  ; если b > c, увеличиваем a и сбрасываем b на a+1
-          (= (+ (* a a) (* b b)) (* c c)) (* a b c)
-          :else (recur a (inc b)))))))
+          (> b c)
+          (recur (inc a) (inc a))  ; увеличиваем a, b = a + 1
+
+          (= (+ (* a a)
+                (* b b))
+             (* c c))
+          (* a b c)
+
+          :else
+          (recur a (inc b)))))))
 
 ;; 3. Модульный подход
 (defn generate-triples []
   (for [a (range 1 334)
         b (range (inc a) 500)
         :let [c (- 1000 a b)]
-        :when (and (> c b) (= (+ (* a a) (* b b)) (* c c)))]
+        :when (and (> c b)
+                   (= (+ (* a a)
+                         (* b b))
+                      (* c c)))]
     [a b c]))
 
 (defn solve-modular []
@@ -43,7 +62,9 @@
                  (map (fn [b]
                         (let [c (- 1000 a b)]
                           (when (and (> c b)
-                                     (= (+ (* a a) (* b b)) (* c c)))
+                                     (= (+ (* a a)
+                                           (* b b))
+                                        (* c c)))
                             (* a b c))))
                       (range (inc a) 500))))
        (remove nil?)
@@ -55,11 +76,20 @@
             (let [c (- 1000 a b)]
               (lazy-seq
                (cond
-                 (> a 333) nil
-                 (> b c) (triples-gen (inc a) (inc (inc a)))
-                 (= (+ (* a a) (* b b)) (* c c))
-                 (cons (* a b c) (triples-gen a (inc b)))
-                 :else (triples-gen a (inc b))))))]
+                 (> a 333)
+                 nil
+
+                 (> b c)
+                 (triples-gen (inc a) (inc (inc a)))
+
+                 (= (+ (* a a)
+                       (* b b))
+                    (* c c))
+                 (cons (* a b c)
+                       (triples-gen a (inc b)))
+
+                 :else
+                 (triples-gen a (inc b))))))]
     (triples-gen 1 2)))
 
 (defn solve-lazy []
